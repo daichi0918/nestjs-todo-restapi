f59a0bc5e9239b92c00111948fa94a25693a0a67
daichi0918 <kayasugadaichi@icloud.com>
2025-01-07 12:04:35 +0900

    update機能delete機能追加

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,11 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './todos.model';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
   @Get()
-  findAll() {
+  findAll(): Todo[] {
     return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Todo {
+    return this.todosService.findById(Number(id));
+  }
+
+  @Post()
+  create(
+    @Body('id') id: number,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): Todo {
+    const todo: Todo = {
+      id,
+      title,
+      content,
+    };
+    return this.todosService.create(todo);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): Todo {
+    const NumId = Number(id);
+    const todo: Todo = {
+      id: NumId,
+      title,
+      content,
+    };
+    return this.todosService.update(NumId, todo);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.todosService.delete(Number(id));
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todos.model';
 
 @Injectable()
 export class TodosService {
-  findAll() {
-    return 'This is TodosService';
+  private todos: Todo[] = [];
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findById(id: number): Todo {
+    return this.todos.find((todo) => todo.id === id);
+  }
+
+  create(todo: Todo): Todo {
+    this.todos.push(todo);
+    return todo;
+  }
+
+  update(id: number, todo: Todo): Todo {
+    const index = this.todos.findIndex((t) => t.id === id);
+    if (index === -1) {
+      throw new Error(`Todo with id ${id} not found`);
+    }
+
+    // 更新対象のTodoを上書き
+    this.todos[index] = { ...this.todos[index], ...todo };
+    return this.todos[index];
+  }
+
+  delete(id: number) {
+    const newTodo = this.todos.filter((todo) => todo.id !== id);
+    this.todos = newTodo;
   }
 }
