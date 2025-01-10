52340717543df946be3ad632679a4404edb61bf3
daichi0918 <kayasugadaichi@icloud.com>
2025-01-10 09:45:26 +0900

    swaggerの実装仮完了

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -9,6 +9,7 @@ import {
   Put,
   UseGuards,
   Request,
+  HttpStatus,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { Todo } from '@prisma/client';
@@ -16,10 +17,68 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Request as ExpressRequest } from 'express';
 import { RequestUser } from 'types/requestUser';
+import { ApiTags, ApiBody, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('todos')
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
+
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string',
+          default: 'test@gmail.com',
+        },
+        password: {
+          type: 'string',
+          default: 'test',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'todo一覧',
+    content: {
+      'application/json': {
+        example: [
+          {
+            id: 7,
+            title: 'Todo5',
+            content: 'Todo5 content',
+            createdAt: '2025-01-09T01:56:41.000Z',
+            updateAt: '2025-01-09T01:56:41.000Z',
+            userId: 5,
+          },
+          {
+            id: 9,
+            title: 'Todo10',
+            content: 'Todo10 content',
+            createdAt: '2025-01-10T00:41:31.000Z',
+            updateAt: '2025-01-10T00:41:31.000Z',
+            userId: 5,
+          },
+        ],
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'todo一覧',
+    content: {
+      'application/json': {
+        example: [
+          {
+            message: 'Unauthorized',
+            statusCode: 401,
+          },
+        ],
+      },
+    },
+  })
   @Get()
   @UseGuards(AuthGuard('jwt'))
   async findAll(
